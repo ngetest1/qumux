1ed85d70dc60883fa8215e750883731f63f0da03
ngetest1 <53402046+ngetest1@users.noreply.github.com>
2019-07-30 20:57:06 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+## QUOTES IN TERMUX
+# Command Install For Termux / Linux (pc)
+```
+$ pkg install php
+$ pkg install git
+$ git clone https://github.com/ngetest1/qumux
+$ cd qumux
+$ ls
+$ php quotes.php
+```
+# Coded By
+Fathur ID  
+
+Decoded From kkktp generator
+
+Team :
+# Newbie Tersakiti Sec7
+
+Blog : https://www.nts-7.net
+
+Thx
---
+++ quotes.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+error_reporting(0);
+require 'var.php';
+echo $cln;
+system("clear");
+kkktp_banner();
+@ini_set('output_buffering',0);
+$data = array('CLBK |  '"CODING LAMA BERES KAGA" |');
+$data2 = $data[array_rand($data)];
+$data3 = explode(' | ', $data2);
+//$ktp = base64_encode($data3[0]);
+//$kk = base64_encode($data3[1]);
+$ktp = $data3[0];
+$kk = $data3[1];
+//$data4 = array('KTP' => $ktp, 'NO_KK' => $kk);
+//print(json_encode(array('src'=>'Hasil', 'result'=>$data4)));
+echo "\e[97m Code By; Fathur ID - NTS7\n";
+echo " Team;\e[92m Newbie Tersakiti Sec7 || www.nts-7.net\n\n";
+echo "~ : ".$ktp."\n";
+echo "~  : ".$kk."\n";
+?>
